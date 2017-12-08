b309dfa7e2cc566f57ae5fc8dbe4c6f3e30dfc16
Jani Anttila <jva@1BQQE25.viikki.dhcp.hupnet.helsinki.fi>
2017-12-08 12:38:12 +0200

    julia added

--- emacs/user/init-languages.el
+++ emacs/user/init-languages.el
@@ -18,6 +18,20 @@
   :ensure t
   :defer t)
 
+(use-package julia-mode
+  :ensure t
+  :defer t)
+
+(use-package julia-shell
+  :ensure t
+  :defer t
+  :config
+  (defun my-julia-mode-hooks ()
+    (require 'julia-shell-mode))
+  (add-hook 'julia-mode-hook 'my-julia-mode-hooks)
+  (define-key julia-mode-map (kbd "C-c C-c") 'julia-shell-run-region-or-line)
+  (define-key julia-mode-map (kbd "C-c C-s") 'julia-shell-save-and-go))
+
 (use-package slime
   :ensure t
   :config
