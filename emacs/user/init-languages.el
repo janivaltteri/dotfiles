64f631a9bc63cbbe3655b13786386093acd67533
Jani Anttila <jva@localhost.localdomain>
2018-05-13 11:25:58 +0300

    removed haskell

--- emacs/user/init-languages.el
+++ emacs/user/init-languages.el
@@ -18,23 +18,6 @@
   :ensure t
   :defer t)
 
-(use-package haskell-mode
-  :ensure t
-  :mode "\\.hs\\'"
-  :commands haskell-mode
-  :bind ("C-c C-s" . fix-imports)
-  :config
-  (custom-set-variables
-   '(haskell-ask-also-kill-buffers nil)
-   '(haskell-process-type (quote stack-ghci))
-   '(haskell-interactive-popup-errors nil))
-  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  (add-hook 'haskell-mode-hook 'hindent-mode)
-  (add-hook 'haskell-mode-hook (lambda ()
-                                 (add-hook 'before-save-hook 'haskell-mode-format-imports nil 'local))))
-
 (use-package julia-mode
   :ensure t
   :defer t
